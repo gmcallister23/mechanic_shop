aa5710a7195a93108fc60e8bd94af92d4a1dad97
Graham McAllister <11613786+gmcallister23@users.noreply.github.com>
2026-05-31 12:44:11 -0400

    set up caching abilities

--- app/extensions.py
+++ app/extensions.py
@@ -1,6 +1,7 @@
 from flask_marshmallow import Marshmallow
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from flask_caching import Cache
 
 
 ma = Marshmallow()
@@ -8,3 +9,4 @@ limiter = Limiter(
     key_func=get_remote_address
     
     )
+cache = Cache()
--- config.py
+++ config.py
@@ -3,6 +3,8 @@
 class DevelopmentConfig:
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Vjzs3455@localhost/mechanic_shop'
     DEBUG = True
+    CACHE_TYPE = 'SimpleCache'
+    CACHE_DEFAULT_TIMEOUT: 300
 
 
 class TestingConfig:
