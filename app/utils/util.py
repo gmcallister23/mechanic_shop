ea8fb792c27e11029bebd0ac160c51e88dfae3b6
Graham McAllister <11613786+gmcallister23@users.noreply.github.com>
2026-05-31 13:46:16 -0400

    setting up token required decorator

--- app/utils/util.py
+++ app/utils/util.py
@@ -1,5 +1,9 @@
 import jwt
 from datetime import datetime, timezone, timedelta
+from functools import wraps
+from flask import request, jsonify
+
+
 
 SECRET_KEY = 'super secret secrets'
 
@@ -12,3 +16,35 @@ def encode_token(customer_id):
 
     token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
     return token
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if 'Authorization' in request.headers:
+
+            token = request.headers('Authorization').split()[1]
+
+            if not token:
+                return jsonify({'message': 'missing token'}), 400
+            
+            try: 
+                data = jwt.decode(token, SECRET_KEY,algorithms='HS256')
+                print(data)
+                customer_id = data('sub')
+            
+            except jwt.ExpiredSignatureError as e: 
+                return jsonify({'message': 'token expired'}), 400
+            
+            except jwt.InvalidTokenError as e:
+                return jsonify({'message': 'Invalid token'}), 400
+            
+            return f(customer_id, *args, **kwargs)
+            
+        else: 
+            return jsonify({'message': 'You must be logged in to access this.'}), 400
+        
+    return decorated
+
+            
