7e700081e7891057b0cb1a26ba48fa2f73494c13
Graham McAllister <11613786+gmcallister23@users.noreply.github.com>
2026-06-04 21:11:57 -0400

    isntalling and setting up swagger for documentation

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,18 @@ from .Blueprints.customers import customers_bp
 from .Blueprints.mechanics import mechanics_bp
 from .Blueprints.service_tickets import service_tickets_bp
 from .Blueprints.inventory import inventory_bp
+from flask_swagger_ui import get_swaggerui_blueprint
+
+SWAGGER_URL = '/api/docs' #URL for exposing swagger UI (without trailing '/')
+API_URL = '/static/swagger.yaml' #Our API URL (can of course be a local source)
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "API NAME"
+    }
+)
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -25,5 +37,6 @@ def create_app(config_name):
     app.register_blueprint(mechanics_bp, url_prefix='/mechanics')
     app.register_blueprint(service_tickets_bp, url_prefix='/service_tickets')
     app.register_blueprint(inventory_bp, url_prefix='/inventory')
+    app.register_blueprint(swaggerui_blueprint, url_prefix='SWAGGER_URL')
 
     return app
