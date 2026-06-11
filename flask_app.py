d906a4551319c23760cdd469b5c9365c926de19a
Graham McAllister <11613786+gmcallister23@users.noreply.github.com>
2026-06-10 21:00:01 -0400

    changed app to flask_app, updated from development config to production config, add psycopg2 to requirements

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,8 @@
 from app import create_app
 from  app.models import db
 
-app = create_app('DevelopmentConfig')
-
+#app = create_app('DevelopmentConfig')
+app = create_app('ProductionConfig')
 def reset_database():
     db.drop_all()
     db.create_all()
